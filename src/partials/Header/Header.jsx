49e54873f57eb0c38f5b215a7edc2b024316c118
arcinston <arushkurundodi@gmail.com>
2021-03-21 16:32:55 +0530

    logo image dont work

--- src/partials/Header/Header.jsx
+++ src/partials/Header/Header.jsx
@@ -1,11 +1,12 @@
 import './Header.css';
 
 //Components;
-
+//import logo from "logo192.png"
 
 export default function Header() {
     return (
         <div>
+            <img src="logo.png" alt="logo" />
             <p>This is header</p>
         </div>
     )
