a487827c6b8097e9a3761b9fac7330c9505f0854
arcinston <arushkurundodi@gmail.com>
2021-03-22 13:56:07 +0530

    v2

--- src/partials/Header/Header.jsx
+++ src/partials/Header/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
     <div className="Header">
       <img src={logo} alt="logo" className="logo" />
       <h1 className="ProjectName">Stonks</h1>
-      <ul>
+      <ul className="NavMenu">
         {NavbarItems.map((item, index) => {return(
           <li>
             <a className={item.cName} href={item.url} >
