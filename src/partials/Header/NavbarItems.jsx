352d6c56b85a8407730f5af26f38c9e2285567d3
arcinston <arushkurundodi@gmail.com>
2021-03-27 00:37:10 +0530

    bc dimaag kharab ho raha hai

--- src/partials/Header/NavbarItems.jsx
+++ src/partials/Header/NavbarItems.jsx
@@ -1,6 +1,6 @@
 const NavbarItems = [
-  { title: "Home", url: "#", cName: "nav-options" },
-  { title: "Dashboard", url: "#", cName: "nav-options" },
-  { title: "About", url: "#", cName: "nav-options" }
+  { title: "Home", url: "stonks-frontend/#", cName: "nav-options" },
+  { title: "Dashboard", url: "stonks-frontend/#", cName: "nav-options" },
+  { title: "About", url: "stonks-frontend/#", cName: "nav-options" },
 ];
 export default NavbarItems
